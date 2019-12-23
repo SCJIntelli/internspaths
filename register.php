5a1ee795c475ebe754fb7004bccdd255b57f1585
ssadhikari <44244091+ssadhikari@users.noreply.github.com>
2019-12-24 01:12:28 +0530

    Update register.php

--- register.php
+++ register.php
@@ -3,7 +3,7 @@
 require_once "config.php";
  
 // Define variables and initialize with empty values
-$username = $password = $confirm_password = $Email= "";
+$username = $password = $confirm_password = $Email= $userType="";
 $username_err = $password_err = $confirm_password_err =$email_err= "";
  
 // Processing form data when form is submitted
@@ -90,19 +90,21 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             }
         }
     }
+    $userType = trim($_POST["user_type"]);
     // Check input errors before inserting in database
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
         
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $sql = "INSERT INTO users (username, password,usertype) VALUES (?, ?,?)";
          
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            mysqli_stmt_bind_param($stmt, "sss", $param_username, $param_password,$param_userType);
             
             // Set parameters
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            $param_userType = $userType;
             
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){

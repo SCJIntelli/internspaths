557d0d398a61e461b6237aa119d02f48cd25bcbb
Shamal Jayathilake <shamalchamara@gmail.com>
2019-12-24 10:18:31 +0530

    svc

--- adminwelcome.php
+++ adminwelcome.php
@@ -1,14 +1,14 @@
 <?php
 // Initialize the session
 session_start();
- 
+
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
 }
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,14 +18,32 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <style type="text/css">
         body{ font: 14px sans-serif; text-align: center; }
     </style>
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
     <div class="page-header">
         <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to Your <b><?php echo htmlspecialchars($_SESSION["usertype"]); ?></b> Control Center.</h1>
     </div>
+    <div class="btn ">
+        <button class="login100-form-btn" class="btn btn-primary" onclick="addAdminBtn()">Add Admin</button>
+    </div>
+    <div id="addAdminForm">
+    </div>
     <p>
-        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="reset-password.php"  class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
     </p>
 </body>
+
+<script >
+    function btnFun1() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+</script>
 </html>
--- login.php
+++ login.php
@@ -1,34 +1,34 @@
 <?php
 // Initialize the session
 session_start();
- 
+
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     if($_SESSION["usertype"]=="Student"){
-    header("location: studentwelcome.php");
-    exit;
+        header("location: studentwelcome.php");
+        exit;
     }
     if($_SESSION["usertype"]=="Company"){
-    header("location: companywelcome.php");
-    exit;
+        header("location: companywelcome.php");
+        exit;
     }
     if($_SESSION["usertype"]=="Admin"){
-    header("location: adminwelcome.php");
-    exit;
+        header("location: adminwelcome.php");
+        exit;
     }
     
 }
- 
+
 // Include config file
 require_once "config.php";
- 
+
 // Define variables and initialize with empty values
 $username = $password = $usertype= "";
 $username_err = $password_err = "";
- 
+
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
+   
     // Check if username is empty
     if(empty(trim($_POST["username"]))){
         $username_err = "Please enter username.";
@@ -46,7 +46,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT id, username, password, usertype FROM users WHERE username = ?";
+        $sql = "SELECT id, username, password, usertype,email FROM users WHERE username = ?";
         
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
@@ -63,7 +63,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 // Check if username exists, if yes then verify password
                 if(mysqli_stmt_num_rows($stmt) == 1){                    
                     // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password, $usertype);
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password, $usertype , $email);
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
                             // Password is correct, so start a new session
@@ -73,86 +73,88 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
                             $_SESSION["username"] = $username; 
-                            $_SESSION["usertype"] = $usertype;                          
+                            $_SESSION["usertype"] = $usertype;
+                            $_SESSION["email"] = $email;                         
                             
                             // Redirect user to welcome page
-                                if($_SESSION["usertype"]=="Student"){
-    header("location: studentwelcome.php");
-    exit;
-    }
-    if($_SESSION["usertype"]=="Company"){
-    header("location: companywelcome.php");
-    exit;
-    }
-    if($_SESSION["usertype"]=="Admin"){
-    header("location: adminwelcome.php");
-    exit;}
-                        } else{
+                            if($_SESSION["usertype"]=="Student"){
+                                header("location: studentwelcome.php");
+                                exit;
+                            }
+                            if($_SESSION["usertype"]=="Company"){
+                                header("location: companywelcome.php");
+                                exit;
+                            }
+                            if($_SESSION["usertype"]=="Admin"){
+                                header("location: adminwelcome.php");
+                                exit;}
+                            } 
+                            else{
                             // Display an error message if password is not valid
-                            $password_err = "The password you entered was not valid.";
+                                $password_err = "The password you entered was not valid.";
+                            }
                         }
+                    } else{
+                    // Display an error message if username doesn't exist
+                        $username_err = "No account found with that username.";
                     }
                 } else{
-                    // Display an error message if username doesn't exist
-                    $username_err = "No account found with that username.";
+                    echo "Oops! Something went wrong. Please try again later.";
                 }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
             }
+            
+        // Close statement
+            mysqli_stmt_close($stmt);
         }
         
-        // Close statement
-        mysqli_stmt_close($stmt);
-    }
-    
     // Close connection
-    mysqli_close($link);
-}
-?>
- 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>InternsPaths</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
-    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->  
-    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->  
-    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="css/util.css">
-    <link rel="stylesheet" type="text/css" href="css/main.css">
-</head>
+        mysqli_close($link);
+    }
+    ?>
+    
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>InternsPaths</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+        <style type="text/css">
+            body{ font: 14px sans-serif; }
+            .wrapper{ width: 350px; padding: 20px; }
+        </style>
+        <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+        <!--===============================================================================================-->  
+        <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+        <!--===============================================================================================-->  
+        <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="css/util.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+    </head>
 
-<body>
-   <div class="limiter">
+    <body>
+     <div class="limiter">
       <div class="container-login100" style="background-image: url('bg-01.jpg')" >
-         <!-- <div class="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30"> -->
-    <div class="wrapper" style="background-color: white;border-radius: 25px;">
-<span class="login100-form-title p-b-70">
-                  Welcome to InternsPaths
-               </span>
-        
-        <p>Please fill in your credentials to login.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+       <!-- <div class="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30"> -->
+        <div class="wrapper" style="background-color: white;border-radius: 25px;">
+            <span class="login100-form-title p-b-70">
+              Welcome to InternsPaths
+          </span>
+          
+          <p>Please fill in your credentials to login.</p>
+          <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                 <label>Username</label>
                 <input type="text" name="username" class="form-control" value="<?php echo $username; ?>" style="border-radius: 25px">
@@ -167,11 +169,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <input type="submit" class="login100-form-btn" class="btn btn-primary"  value="Login">
             </div>
             <p style="text-align: center;"><span class="txt1">
-                        Don’t have an account?
-                     </span> <a href="register.php" class="txt2">Sign up now</a>.</p>
+                Don’t have an account?
+            </span> <a href="register.php" class="txt2">Sign up now</a>.</p>
         </form>
-<!--     </div> -->    
- </div>
+        <!--     </div> -->    
+    </div>
 </div>
 </div>
 </body>

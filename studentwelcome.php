9b9b135c72106dfdbb6647b1ccb101d6282fb78a
ssadhikari <44244091+ssadhikari@users.noreply.github.com>
2019-12-24 12:52:26 +0530

    Update studentwelcome.php

--- studentwelcome.php
+++ studentwelcome.php
@@ -19,20 +19,47 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <style type="text/css">
         body{ font: 14px sans-serif; text-align: center; }
     </style>
+      <!--===============================================================================================-->  
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->  
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->  
+    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+    
 </head>
 <body>
-    <div class="page-header">
-        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to Your <b><?php echo htmlspecialchars($_SESSION["usertype"]); ?></b> Control Center.</h1>
-    </div>
-    <p>
-        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
-        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
-    </p>
-    <div>
-        <p ><span style="lef"><?php echo $_SESSION["username"]?></span></p>
-        <div class="wrapper" style="border-radius: 50px;background-color: blue;">
-            <input type="submit"  class="login100-form-btn"   value="edit your profile" style="width: 45% ;">
-            <input type="reset" class="login100-form-btn" value="Reset" style="width: 45%;position:relative; top:-50px;left:170px">
+    <div class="limiter">
+        <div class="container-login100" style="background-image: url('back2.png');background-size:cover; ">
+            <div class="page-header">
+                <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to Your <b><?php echo htmlspecialchars($_SESSION["usertype"]); ?></b> Control Center.</h1>
+            </div>
+            <br>
+            <p>
+                <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+                <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+            </p>
+            
+            <br>   
+            <div class="wrapper" style="border-radius: 50px;background-color: blue;">
+                <p ><span style="lef"><?php echo $_SESSION["username"]?></span></p>
+            </div>
+            
         </div>
     </div>
 </body>

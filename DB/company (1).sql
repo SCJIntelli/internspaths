d6ebb92c92043e519ab2999ecf0d6f5837dc7b7f
Shamal Jayathilake <shamalchamara@gmail.com>
2020-01-30 13:15:11 +0530

    scj

--- DB/company (1).sql
+++ DB/company (1).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jan 29, 2020 at 08:40 PM
--- Server version: 10.4.10-MariaDB
--- PHP Version: 7.3.12
+-- Generation Time: Jan 30, 2020 at 07:44 AM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `company` (
   `facebook` varchar(1000) DEFAULT NULL,
   `linkedin` varchar(1000) DEFAULT NULL,
   `twitter` varchar(1000) DEFAULT NULL,
-  `fields` varchar(1000) NOT NULL,
+  `fields` varchar(1000) NOT NULL DEFAULT '',
   `mission` varchar(5000) DEFAULT NULL,
   `vision` varchar(5000) DEFAULT NULL,
   `requests` varchar(1000) NOT NULL DEFAULT '',
@@ -57,7 +57,7 @@ CREATE TABLE IF NOT EXISTS `company` (
 --
 
 INSERT INTO `company` (`comnum`, `id`, `username`, `email`, `name`, `profileurl`, `mobile`, `address`, `description`, `location`, `facebook`, `linkedin`, `twitter`, `fields`, `mission`, `vision`, `requests`, `applied`) VALUES
-(1, 5, 'Company', 'internspaths@mil.lk', 'MIT', '../uploads/5.jpg', '0766573602', 'asdasdasdasd', 'The company summary in a business planï¿½also known as the company description or overviewï¿½is a high-level look at what you are as a company and how all the elements of the business fit together. An effective company summary should give readers, such as potential investors, a quick and easy way to understand your business, its products and services, its mission and goals, how it meets the needs of its target market, and how it stands out from competitors.\r\n\r\n\r\nBefore you begin writing your company summary, remember to stick to the big picture. Other sections of your business plan will provide the specific details of your business. The summary synthesizes all of that information into one page.', 'bla', 'facebook', '', '', 'java,css,machine learning,web designing,app developing', 'To provide superior quality healthcare services that: PATIENTS recommend to family and friends, PHYSICIANS prefer for their patients, PURCHASERS select for their clients, EMPLOYEES are proud of, and INVESTORS seek for long-term returns.', 'â€œTo become the worldâ€™s most loved, most flown, and most profitable airline.â€', '4,', ''),
+(1, 5, 'Company', 'internspaths@mil.lk', 'MIT', '../uploads/5.jpg', '0766573602', 'asdasdasdasd', 'The company summary in a business planï¿½also known as the company description or overviewï¿½is a high-level look at what you are as a company and how all the elements of the business fit together. An effective company summary should give readers, such as potential investors, a quick and easy way to understand your business, its products and services, its mission and goals, how it meets the needs of its target market, and how it stands out from competitors.\r\n\r\n\r\nBefore you begin writing your company summary, remember to stick to the big picture. Other sections of your business plan will provide the specific details of your business. The summary synthesizes all of that information into one page.', 'bla', 'facebook', '', '', 'java,css,machine learning,web designing,app developing', 'To provide superior quality healthcare services that: PATIENTS recommend to family and friends, PHYSICIANS prefer for their patients, PURCHASERS select for their clients, EMPLOYEES are proud of, and INVESTORS seek for long-term returns.', 'â€œTo become the worldâ€™s most loved, most flown, and most profitable airline.â€', '4,', ','),
 (2, 12, 'COM2', 'wew@dwa.rad', 'Unknown', '../images/avatar-01.jpg', 'Unknown', 'Unknown', 'no description', '#', '#', '#', '#', '', NULL, '', '4,', ''),
 (3, 17, 'Shamal12312', '122323@123123123.com', 'Unknown', '../images/avatar-01.jpg', 'Unknown', 'Unknown', 'no description', '#', '#', '#', '#', '', NULL, '', '4,', '');
 COMMIT;

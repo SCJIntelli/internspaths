269dc9c495b34badfd4a7731380ae32d98fc6c84
IsuruJayaweera <59163082+IsuruJayaweera@users.noreply.github.com>
2020-01-30 14:45:11 +0530

    aa

--- student/receivedrequests.php
+++ student/receivedrequests.php
@@ -167,7 +167,7 @@ if($result = mysqli_query($link, $sql)){
                   <li><a href="editmyprofile.php?id=<?php echo $_SESSION["id"]?>"><i class="fa fa-cogs"></i>Edit My Profile</a></li>
                   <li ><a href="searchcompanies.php"><i class="fa fa-search"></i>Search Companies</a></li>
                   <li><a href="viewrequests.php"><i class="fa fa-send"></i>Sent Requests</a></li>
-                  <li class= "active"><a href="receivedrequests.php"><i class="fa fa-send"></i>Received Requests</a></li>
+                  <li class= "active"><a href="receivedrequests.php"><i class="fa fa-bell"></i>Received Requests</a></li>
                   <li><a href="security.php"><i class="fa fa-lock"></i>Security</a></li>
 
 
@@ -245,7 +245,7 @@ if($result = mysqli_query($link, $sql)){
                     echo "<td>" . $row['email'] . "</td>";
                     echo "<td>" . $row['mobile'] . "</td>";
                     echo "<td>";
-                    echo "<a href='viewcompanyvr.php?id=". $row['id'] ."' title='View Record' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span> &nbsp;&nbsp; </a>";
+                    echo "<a href='viewcompanyrr.php?id=". $row['id'] ."' title='View Record' data-toggle='tooltip'><span class='glyphicon glyphicon-eye-open'></span> &nbsp;&nbsp; </a>";
                                 // echo "<a href='editcompany.php?id=". $row['id'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span> &nbsp;&nbsp;  </a>";
                                 // echo "<a href='deletecom.php?id=". $row['id'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span>&nbsp;&nbsp;&nbsp;&nbsp;</a>";
                                 //  echo "<a href='reset-password.php?id=". $row['id'] ."&return=managecompany.php' title='Reset Password' data-toggle='tooltip'><span class='glyphicon glyphicon-link'></span> &nbsp;&nbsp; </a>";
